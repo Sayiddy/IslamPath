5424aaf8c0232de61d5941217a0ee65d6287b9d0
Sayiddy <Sayidkamol2009@gmail.com>
2025-12-15 14:18:36 +0800

    colors

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Btn({className, children}) {
   return (
-    <Link to="/trial-class" className={"bg-transparent outline-[#FCCD2A] outline-3 hover:bg-[#C28927] hover:outline-none hover:text-white text-[#131313] text-center px-4 py-2 rounded-[15px] transition-all duration-300 " + className}>
+    <Link to="/trial-class" className={"bg-transparent outline-cYellow outline-3 hover:bg-cBronze hover:outline-none hover:text-white text-cBlack text-center px-4 py-2 rounded-[15px] transition-all duration-300 " + className}>
       {children}
     </Link>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,13 +8,13 @@ export default function NavBar({ currentPage }) {
     <div className="w-full h-[100px] bg-white align-middle sticky top-0 shadow-md flex items-center">
       <img src={logoR} alt="Islam Path Logo" className="h-[50px] m-[25px] ml-[50px] mr-auto"/>
       <div className='grid grid-rows-1 grid-flow-col gap-10 m-[25px]'>
-        <Link to="/" className={'text-[18px] ' + (currentPage === "Home" ? "text-[#FCCD2A]" : " hover:text-[#FCCD2A]")}>Home</Link>
-        <Link to="/about-us" className={'text-[18px] ' + (currentPage === "About Us" ? "text-[#FCCD2A]" : " hover:text-[#FCCD2A]")}>About Us</Link>
-        <Link to="/courses" className={'text-[18px] ' + (currentPage === "Courses" ? "text-[#FCCD2A]" : " hover:text-[#FCCD2A]")}>Courses</Link>
-        <Link to="/tutors" className={'text-[18px] ' + (currentPage === "Tutors" ? "text-[#FCCD2A]" : " hover:text-[#FCCD2A]")}>Tutors</Link>
-        <Link to="/contact-us" className={'text-[18px] ' + (currentPage === "Contact Us" ? "text-[#FCCD2A]" : " hover:text-[#FCCD2A]")}>Contact Us</Link>
-        <Link to="/pricing" className={'text-[18px] ' + (currentPage === "Pricing" ? "text-[#FCCD2A]" : " hover:text-[#FCCD2A]")}>Pricing</Link>
-        <Link to="/blog" className={'text-[18px] ' + (currentPage === "Blog" ? "text-[#FCCD2A]" : " hover:text-[#FCCD2A]")}>Blog</Link>
+        <Link to="/" className={'text-[18px] ' + (currentPage === "Home" ? "text-cYellow" : " hover:text-cYellow")}>Home</Link>
+        <Link to="/about-us" className={'text-[18px] ' + (currentPage === "About Us" ? "text-cYellow" : " hover:text-cYellow")}>About Us</Link>
+        <Link to="/courses" className={'text-[18px] ' + (currentPage === "Courses" ? "text-cYellow" : " hover:text-cYellow")}>Courses</Link>
+        <Link to="/tutors" className={'text-[18px] ' + (currentPage === "Tutors" ? "text-cYellow" : " hover:text-cYellow")}>Tutors</Link>
+        <Link to="/contact-us" className={'text-[18px] ' + (currentPage === "Contact Us" ? "text-cYellow" : " hover:text-cYellow")}>Contact Us</Link>
+        <Link to="/pricing" className={'text-[18px] ' + (currentPage === "Pricing" ? "text-cYellow" : " hover:text-cYellow")}>Pricing</Link>
+        <Link to="/blog" className={'text-[18px] ' + (currentPage === "Blog" ? "text-cYellow" : " hover:text-cYellow")}>Blog</Link>
       </div>
       <Btn className="ml-auto mr-[50px] text-[18px] px-7 py-2.5">Book Trial Class</Btn>
     </div>
