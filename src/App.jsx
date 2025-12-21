03732d5a55249ae6b9dfb1bc09a199a17814ae6a
Sayiddy <Sayidkamol2009@gmail.com>
2025-12-21 09:08:36 +0800

    hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './routes/Home.jsx'
 import NavBar from './components/NavBar.jsx'
 import Footer from './components/Footer.jsx'
