97e33f86575c957c70d2b8a83d20ddf16970018d
Sayiddy <Sayidkamol2009@gmail.com>
2025-12-24 08:37:53 +0800

    footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,14 @@ import goldPattern from '../assets/pngtree-circle-islamic-pattern-green-gold-lux
 
 export default function Footer() {
   return(
-    <div className="relative overflow-hidden h-[550px] w-full bg-cDarkerGreen text-center text-white flex flex-row justify-center items-center text-[18px] gap-20">
+    <div className="relative overflow-hidden h-fit sm:h-[550px] py-10 w-full bg-cDarkerGreen text-center text-white flex flex-row justify-center items-center text-[18px] gap-20 w-full bottom-0">
       {/* decorative background patterns */}
       <img src={goldPattern} alt="" className="absolute left-0 bottom-0 opacity-30 z-0 pointer-events-none size-44" style={{transform: 'translate(55%, 50%)'}}/>
       <img src={goldPattern} alt="" className="absolute left-0 bottom-0 opacity-30 z-0 pointer-events-none size-64" style={{transform: 'translate(-50%, 30%)'}}/>
       <img src={goldPattern} alt="" className="absolute left-0 bottom-0 opacity-15 z-0 pointer-events-none size-28" style={{transform: 'translate(0%, -100%)'}}/>
       <img src={goldPattern} alt="" className="absolute right-0 top-0 opacity-22 z-0 pointer-events-none size-60 rotate-180" style={{transform: 'translate(-40%, -40%)'}}/>
       <img src={goldPattern} alt="" className="absolute right-0 top-0 opacity-17 z-0 pointer-events-none size-44 rotate-180" style={{transform: 'translate(30%, 35%)'}}/>
-      <div className="flex flex-col z-10">
+      <div className="hidden sm:flex flex-col z-10">
         <img src={logo} alt="Islam Path Logo" className="h-36 m-2.5 mx-auto rounded-2xl"/>
         <p className='font-semibold mt-5'>
           Deepen your connection <br/>
