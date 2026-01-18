4ee1d3fcc2a25f402ceaffcffffe77834ceeee6b
Sayiddy <Sayidkamol2009@gmail.com>
2026-01-18 21:47:32 +0800

    fix

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -20,9 +20,9 @@ export default function SideBar({setSideBarOpen}) {
 
         <div className="bg-black h-0.5 opacity-5"></div>
 
-        <div to="/about-us" className="h-15 flex">
+        <Link to="/about-us" className="h-15 flex">
           <p className={'text-[18px] text-cGery my-auto ml-5'}>About Us</p>
-        </div>
+        </Link>
         
         <div className="bg-black h-0.5 opacity-5"></div>
 
